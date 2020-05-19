fc95660965ea30096d39059736c6465ed5324502
Sven Jens <sven@sven-jens.nl>
2020-05-19 22:07:52 +0200

    added a loading inc

--- apps/client/src/components/Visual.js
+++ apps/client/src/components/Visual.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Figure, Img, FigCaption } from "./VisualStyles";
+import { Card } from "@datapunt/asc-ui";
 
-export default ({ title, ...rest }) => (
-  <Figure>
-    <Img {...rest} />
-    {title && <FigCaption>{title}</FigCaption>}
-  </Figure>
-);
+export default ({ title, ...rest }) => {
+  const [loading, setLoading] = useState(true);
+  return (
+    <Card isLoading={loading}>
+      <Figure>
+        <Img {...rest} onLoad={() => setLoading(false)} />
+        {title && <FigCaption>{title}</FigCaption>}
+      </Figure>
+    </Card>
+  );
+};
