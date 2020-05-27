e20d2ad5315096cc4d13c2ebcce21c2a4ab45e0b
Andre <afj.lambert@gmail.com>
2020-05-27 09:20:25 +0200

    WIP: Implement lazy page loading. Tests are broken.

--- apps/client/src/components/Router.js
+++ apps/client/src/components/Router.js
@@ -1,23 +1,26 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 
 import { routeConfig, redirectConfig } from "../routes";
 import ScrollToTop from "./ScrollToTop";
+import LoadingPage from "../pages/LoadingPage";
 
 const Router = (props) => {
   return (
     <BrowserRouter>
       <ScrollToTop />
-      <Switch>
-        {redirectConfig.map(([from, to]) => (
-          <Redirect key={from} {...{ from, to }} />
-        ))}
-        {routeConfig
-          .filter((route) => route.component)
-          .map((route, i) => (
-            <Route key={i} {...route} />
+      <Suspense fallback={<LoadingPage />}>
+        <Switch>
+          {redirectConfig.map(([from, to]) => (
+            <Redirect key={from} {...{ from, to }} />
           ))}
-      </Switch>
+          {routeConfig
+            .filter((route) => route.component)
+            .map((route, i) => (
+              <Route key={i} {...route} />
+            ))}
+        </Switch>
+      </Suspense>
     </BrowserRouter>
   );
 };
