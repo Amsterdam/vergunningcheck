fb9cb149f3ef32226e28dba7fc351abe2ef4e34b
Sven Jens <sven@sven-jens.nl>
2020-06-10 11:20:14 +0200

    feat: added a error on image (#120)
    feat: added an error state to images

--- apps/client/src/components/Visual.js
+++ apps/client/src/components/Visual.js
@@ -3,10 +3,20 @@ import { Figure, Img, FigCaption } from "./VisualStyles";
 
 export default ({ title, ...rest }) => {
   const [loaded, setLoaded] = useState(false);
+  const [errored, setErrored] = useState(false);
   return (
-    <Figure loaded={loaded}>
-      <Img {...rest} loaded={loaded} onLoad={() => setLoaded(true)} />
-      {title && <FigCaption>{title}</FigCaption>}
+    <Figure loaded={loaded} errored={errored}>
+      <Img
+        {...rest}
+        loaded={loaded}
+        errored={errored}
+        onLoad={() => setLoaded(true)}
+        onError={() => {
+          setLoaded(true);
+          setErrored(true);
+        }}
+      />
+      {title && !errored && <FigCaption>{title}</FigCaption>}
     </Figure>
   );
 };
