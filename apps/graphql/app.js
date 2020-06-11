93ae6decfb50cfca51ee7eb145a27396c69ea500
André Lambert <afj.lambert@gmail.com>
2020-06-11 17:21:58 +0200

    pkg issue?

--- apps/graphql/app.js
+++ apps/graphql/app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const config = require("config");
+const config = require("./node_modules/config");
 const cors = require("cors");
 const morgan = require("morgan");
 const helmet = require("helmet");
