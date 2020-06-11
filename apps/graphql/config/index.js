97245613558d3e0a9e08ccb7ab280075ac45a730
André Lambert <afj.lambert@gmail.com>
2020-06-11 21:28:31 +0200

    Deploy it

--- apps/graphql/config/index.js
+++ apps/graphql/config/index.js
@@ -1,3 +1,5 @@
 console.log("config_file", process.env.CONFIG_FILE);
 
-module.exports = require(`./${process.env.CONFIG_FILE || "production"}`);
+module.exports = require(`./${
+  process.env.CONFIG_FILE || process.env.NODE_ENV
+}`);
---
+++ apps/graphql/config/netlify.js
@@ -0,0 +1,10 @@
+const merge = require("lodash/merge");
+
+const base = require("./default");
+
+const config = {
+  cache: {
+    redis: false,
+  },
+};
+module.exports = merge(base, config);
