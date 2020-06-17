e53d33fb74e56d985a54d1fa937a696f10544669
Sven Jens <sven@sven-jens.nl>
2020-06-17 17:03:37 +0200

    added the enter from dev to localstorage

--- apps/client/src/pages/DevHomePage.js
+++ apps/client/src/pages/DevHomePage.js
@@ -6,32 +6,37 @@ import { routes, geturl } from "../routes";
 
 import Layout from "../components/Layouts/DefaultLayout";
 
-const DevHomePage = () => (
-  <Layout heading="Hi Dev!">
-    <p>Welcome to CHAPPIE 1.0</p>
-    <table>
-      <thead>
-        <tr>
-          <th>Flow</th>
-          <th>slug</th>
-        </tr>
-      </thead>
-      <tbody>
-        {topics
-          .sort((a) => !!a.sttrFile && -1) // make sure sttr flow is on top
-          .map(({ slug, sttrFile, redirectToOlo }) => (
-            <tr key={slug}>
-              <td>
-                {redirectToOlo ? "Redirect" : sttrFile ? "Checker" : "OLO"}
-              </td>
-              <td>
-                <Link to={geturl(routes.intro, { slug })}>{slug}</Link>
-              </td>
-            </tr>
-          ))}
-      </tbody>
-    </table>
-  </Layout>
-);
+const DevHomePage = () => {
+  if (!localStorage.getItem("enterFromDev")) {
+    localStorage.setItem("enterFromDev", "true");
+  }
+  return (
+    <Layout heading="Hi Dev!">
+      <p>Welcome to CHAPPIE 1.0</p>
+      <table>
+        <thead>
+          <tr>
+            <th>Flow</th>
+            <th>slug</th>
+          </tr>
+        </thead>
+        <tbody>
+          {topics
+            .sort((a) => !!a.sttrFile && -1) // make sure sttr flow is on top
+            .map(({ slug, sttrFile, redirectToOlo }) => (
+              <tr key={slug}>
+                <td>
+                  {redirectToOlo ? "Redirect" : sttrFile ? "Checker" : "OLO"}
+                </td>
+                <td>
+                  <Link to={geturl(routes.intro, { slug })}>{slug}</Link>
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </Layout>
+  );
+};
 
 export default DevHomePage;
