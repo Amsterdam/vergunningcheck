92f1ce1b55ebac6c95213eb0b4fa0c22231edcca
André Lambert <afj.lambert@gmail.com>
2020-07-08 15:58:41 +0200

    Fix typo

--- apps/client/src/sttr_client/models/question.js
+++ apps/client/src/sttr_client/models/question.js
@@ -1,6 +1,7 @@
-import isString from "lodash.isstring";
-import { collectionOfType } from "../../utils";
 import isNumber from "lodash.isnumber";
+import isString from "lodash.isstring";
+
+import { collectionOfType } from "../../utils";
 
 const DESC_MAX_LENGTH = 2048;
 export const questionTypes = ["boolean", "string", "number", "geo"];
@@ -31,7 +32,7 @@ class Question {
   // private __type: string;
 
   /**
-   * @param {string} type - datatype of the question, eg. 'boolean', or 'geo'
+   * @param {string} type - data type of the question, eg. 'boolean', or 'geo'
    * @param {string} text - the question itself
    * @param {string} [description] - a description for this question (mind the max-length)
    * @param {string} [longDescription] - a longer description for this question (mind the max-length)
