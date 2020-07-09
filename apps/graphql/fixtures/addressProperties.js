acf862eabda2a13aee7f552142797a4c7839ff0b
André Lambert <afj.lambert@gmail.com>
2020-07-09 09:36:18 +0200

    Correct some typo's

--- apps/graphql/fixtures/addressProperties.js
+++ apps/graphql/fixtures/addressProperties.js
@@ -13,22 +13,22 @@ const fixtures = [
   ["1012TK", "2 C", [CITY_SCAPE]],
   ["1072AX", "239 1", [CITY_SCAPE]],
 
-  // Only national heritage
+  // Only national monument
   ["1075AV", 18, [NATIONAL_MONUMENT]],
   ["1095LJ", 633, [NATIONAL_MONUMENT]],
   ["1054EX", 22, [NATIONAL_MONUMENT]],
 
-  // Only municipal heritage
+  // Only municipal monument
   ["1055XD", "19 c", [MUNICIPAL_MONUMENT]],
   ["1073TK", 149, [MUNICIPAL_MONUMENT]],
   ["1071GM", 124, [MUNICIPAL_MONUMENT]],
 
-  // Municipal heritage and city scape
+  // Municipal monument and city scape
   ["1073HL", "2 1", [MUNICIPAL_MONUMENT, CITY_SCAPE]],
   ["1078AW", 157, [MUNICIPAL_MONUMENT, CITY_SCAPE]],
   ["1072RV", 21, [MUNICIPAL_MONUMENT, CITY_SCAPE]],
 
-  // National heritage and city scape
+  // National monument and city scape
   ["1012TK", "1 A", [NATIONAL_MONUMENT, CITY_SCAPE]],
   ["1017GW", "279 H", [NATIONAL_MONUMENT, CITY_SCAPE]],
   ["1073JP", 1, [NATIONAL_MONUMENT, CITY_SCAPE]],
