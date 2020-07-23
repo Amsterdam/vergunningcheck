696167df7845344dde4dfd244277fcda55451b1a
Robin Piets <robinpiets@gmail.com>
2020-07-23 12:51:41 +0200

    Updated tests to get 100% test coverage

--- apps/client/src/components/StepByStepNavigation/StepByStepNavigation.test.tsx
+++ apps/client/src/components/StepByStepNavigation/StepByStepNavigation.test.tsx
@@ -1,3 +1,6 @@
+import "jest-styled-components";
+
+import { ascDefaultTheme, themeColor, themeSpacing } from "@datapunt/asc-ui";
 import React from "react";
 
 import { STEPBYSTEPITEM, STEPBYSTEPNAVIGATION } from "../../utils/test-ids";
@@ -5,6 +8,13 @@ import { act, cleanup, fireEvent, render } from "../../utils/test-utils";
 import { StepByStepItem, StepByStepNavigation } from ".";
 
 const onClickMock = jest.fn();
+
+const styles = {
+  blue: "#004699",
+  gray: "#B4B4B4",
+  large: "24px",
+  small: "18px",
+};
 
 afterEach(cleanup);
 
@@ -73,13 +83,6 @@ describe("StepByStepNavigation", () => {
   });
 
   it("should render different states correctly", () => {
-    const styles = {
-      blue: "#004699",
-      gray: "#B4B4B4",
-      large: "24px",
-      small: "18px",
-    };
-
     const { queryByTestId, container } = render(
       <StepByStepNavigation>
         <StepByStepItem data-testid="checked" heading="checked" checked />
@@ -92,25 +95,25 @@ describe("StepByStepNavigation", () => {
     // The `checked` circle should be styled small and blue
     const checkedItem = queryByTestId("checked");
     const checkedCircle = checkedItem.querySelector("span");
-    expect(checkedCircle).toHaveStyle(`width: ${styles.small}`);
-    expect(checkedCircle).toHaveStyle(`background-color: ${styles.blue}`);
+    expect(checkedCircle).toHaveStyleRule("width", styles.small);
+    expect(checkedCircle).toHaveStyleRule("background-color", styles.blue);
 
     // The `done` circle should be styled small and blue
     const doneCircle = queryByTestId("done").querySelector("span");
-    expect(doneCircle).toHaveStyle(`width: ${styles.small}`);
-    expect(doneCircle).toHaveStyle(`background-color: ${styles.blue}`);
+    expect(doneCircle).toHaveStyleRule("width", styles.small);
+    expect(doneCircle).toHaveStyleRule("background-color", styles.blue);
 
     // The `active` circle should be styled large and blue
     const activeItem = queryByTestId("active");
     const activeCircle = activeItem.querySelector("span");
-    expect(activeCircle).toHaveStyle(`width: ${styles.large}`);
-    expect(activeCircle).toHaveStyle(`background-color: ${styles.blue}`);
+    expect(activeCircle).toHaveStyleRule("width", styles.large);
+    expect(activeCircle).toHaveStyleRule("background-color", styles.blue);
 
     // The `disabled` circle should be styled small and gray
     const disabledItem = queryByTestId("disabled");
     const disabledCircle = disabledItem.querySelector("span");
-    expect(disabledCircle).toHaveStyle(`width: ${styles.small}`);
-    expect(disabledCircle).toHaveStyle(`background-color: ${styles.gray}`);
+    expect(disabledCircle).toHaveStyleRule("width", styles.small);
+    expect(disabledCircle).toHaveStyleRule("background-color", styles.gray);
 
     // Only the `checked` item should render the `<svg />`
     expect(container.querySelectorAll("svg").length).toEqual(1);
@@ -123,5 +126,69 @@ describe("StepByStepNavigation", () => {
     // Only the `disabled` item should have a `disabled` attribute
     expect(container.querySelectorAll("[disabled]").length).toEqual(1);
     expect(disabledItem).toHaveAttribute("disabled");
+  });
+
+  it("should render customSize states correctly", () => {
+    const { queryByTestId } = render(
+      <StepByStepNavigation cdata-testid="wrapper" customSize highlightActive>
+        <StepByStepItem data-testid="small" heading="small" done />
+        <StepByStepItem data-testid="large" heading="large" largeCircle />
+        <StepByStepItem data-testid="active" heading="active" active />
+        <StepByStepItem
+          data-testid="color"
+          heading="color"
+          circleBackgroundColor="red"
+        />
+        <StepByStepItem
+          data-testid="inactiveWithChildren"
+          heading="inactiveWithChildren"
+        >
+          <span>Child</span>
+        </StepByStepItem>
+        <StepByStepItem
+          data-testid="activeWithChildren"
+          heading="activeWithChildren"
+          largeCircle
+        >
+          <span>Child</span>
+        </StepByStepItem>
+      </StepByStepNavigation>
+    );
+
+    // The `small` circle should be styled small and blue
+    const checkedItem = queryByTestId("small");
+    const checkedCircle = checkedItem.querySelector("span");
+    expect(checkedCircle).toHaveStyleRule("width", styles.small);
+    expect(checkedCircle).toHaveStyleRule("background-color", styles.blue);
+
+    // The `large` circle should be styled large and gray
+    const doneCircle = queryByTestId("large").querySelector("span");
+    expect(doneCircle).toHaveStyleRule("width", styles.large);
+    expect(doneCircle).toHaveStyleRule("background-color", styles.gray);
+
+    // The `color` circle should be styled with a custom color
+    const colorCircle = queryByTestId("color").querySelector("span");
+    expect(colorCircle).toHaveStyleRule("background-color", "red");
+
+    // The `active` item should be styled with a custom background
+    expect(queryByTestId("active")).toHaveStyleRule(
+      "background-color",
+      themeColor("tint", "level2")({ theme: ascDefaultTheme })
+    );
+
+    // Make sure the Heading `gutterBottom` is working
+    expect(
+      queryByTestId("inactiveWithChildren").querySelector("p")
+    ).toHaveStyleRule(
+      "margin-bottom",
+      themeSpacing(3)({ theme: ascDefaultTheme })
+    );
+
+    expect(
+      queryByTestId("activeWithChildren").querySelector("h3")
+    ).toHaveStyleRule(
+      "margin-bottom",
+      themeSpacing(3)({ theme: ascDefaultTheme })
+    );
   });
 });
