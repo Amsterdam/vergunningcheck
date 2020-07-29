70bad2f8abbc6bc470b53da32d9fd91b5af45b7d
Sven Jens <sven@sven-jens.nl>
2020-07-29 13:17:16 +0200

    added refactor logic

--- apps/client/src/pages/WrapperPage.js
+++ apps/client/src/pages/WrapperPage.js
@@ -4,6 +4,7 @@ import { Helmet } from "react-helmet";
 import Conclusion from "../components/Conclusion";
 import DebugDecisionTable from "../components/DebugDecisionTable";
 import Layout from "../components/Layouts/DefaultLayout";
+import Link from "../components/Link";
 import Location from "../components/Location/Location";
 import Questions from "../components/Questions";
 import {
@@ -16,13 +17,49 @@ import withChecker from "../hoc/withChecker";
 const WrapperPage = ({ checker, topic }) => {
   const sessionContext = useContext(SessionContext);
   const { slug, sttrFile } = topic;
-  const { finishedLocation, finishedQuestions } = sessionContext[slug] || false;
+  const {
+    finishedLocation,
+    finishedQuestions,
+    activeComponents,
+    finishedComponents,
+  } = sessionContext[slug];
+
+  const setActiveState = (component, value) => {
+    let setActiveComponents = activeComponents || [];
+    if (!value) {
+      setActiveComponents.filter((c) => c !== component);
+    } else {
+      setActiveComponents.push(component);
+    }
+    sessionContext.setSessionData([
+      slug,
+      { activeComponents: setActiveComponents },
+    ]);
+  };
+  const setFinishedState = (component, value) => {
+    let setFinishedComponents = finishedComponents || [];
+    if (!value) {
+      setFinishedComponents.filter((c) => c !== component);
+    } else {
+      setFinishedComponents.push(component);
+    }
+    sessionContext.setSessionData([
+      slug,
+      { finishedComponents: setFinishedComponents },
+    ]);
+  };
 
   return (
     <Layout>
       <Helmet>
         <title>Wrapper Page</title>
       </Helmet>
+      <Link onClick={() => setActiveState("locationInput", false)}>
+        set active state
+      </Link>
+      <Link onClick={() => setFinishedState("locationInput", true)}>
+        setFinishedState
+      </Link>
       <StepByStepNavigation
         customSize
         disabledTextColor="inherit"
