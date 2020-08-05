2777d86b79c9c0e5aaad32cc23df676f25f36875
Robin Piets <robinpiets@gmail.com>
2020-08-05 13:14:01 +0200

    Refactor the getOloUrl to generateOloUrl in config

--- apps/client/src/components/Location/Address.js
+++ apps/client/src/components/Location/Address.js
@@ -1,7 +1,7 @@
 import { Paragraph } from "@datapunt/asc-ui";
 import React, { useContext } from "react";
 
-import { OLO } from "../../config";
+import { generateOloUrl } from "../../config";
 import { SessionContext } from "../../context";
 import { ADDRESS_PAGE } from "../../utils/test-ids";
 import Form from "../Form";
@@ -13,30 +13,13 @@ const Address = ({ topic, setFinishedState, setActiveState, isFinished }) => {
   const address = sessionContext[topic.slug].address || {};
   const useSTTR = !!topic.sttrFile;
 
-  const getOloUrl = ({ postalCode, houseNumberFull, houseNumber }) => {
-    // Form is validated, we can proceed
-
-    // Generate OLO parameter "postalCode"
-    const oloPostalCode = `facet_locatie_postcode=${postalCode}`;
-
-    // Generate OLO parameter "streetNumber"
-    const oloStreetNumber = `facet_locatie_huisnummer=${houseNumber}`;
-
-    const oloSuffix = `facet_locatie_huisnummertoevoeging=${houseNumberFull
-      .replace(houseNumber, "")
-      .trim()}`;
-
-    // Redirect user to OLO with all parameters
-    return `${OLO.location}?param=postcodecheck&${oloPostalCode}&${oloStreetNumber}&${oloSuffix}`;
-  };
-
   const onSubmit = (e) => {
     e.preventDefault();
     if (useSTTR) {
       setFinishedState("address", true);
       setActiveState("questions");
     } else {
-      window.open(getOloUrl(address), "_blank");
+      window.open(generateOloUrl(address), "_blank");
     }
   };
 
