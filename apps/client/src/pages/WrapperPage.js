461c4fbf3c33af223ff385fb24ccbf919f6e7cf0
Sven Jens <sven@sven-jens.nl>
2020-08-10 21:25:28 +0200

    removed > 0 check

--- apps/client/src/pages/WrapperPage.js
+++ apps/client/src/pages/WrapperPage.js
@@ -110,7 +110,7 @@ const WrapperPage = ({ checker, topic, resetChecker }) => {
             <StepByStepItem
               checked={isFinished("questions")}
               customSize
-              done={isFinished("locationResult") || checker.stack.length > 0}
+              done={isFinished("locationResult") || checker.stack.length}
               heading="Vragen"
               largeCircle
               // Overwrite the line between the Items
