fc611c86904a73a207f41b2f6d016633c54cabad
Robin Piets <robinpiets@gmail.com>
2020-08-17 18:22:07 +0200

    Feature: Added ConclusionAlert for Contact Conclusion

--- apps/client/src/components/ConclusionAlertStyles.ts
+++ apps/client/src/components/ConclusionAlertStyles.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 
 import Alert from "../atoms/Alert";
 
-export const PermitAlertStyle = styled(Alert)`
+export const ConclusionAlertStyle = styled(Alert)`
   margin-top: ${themeSpacing(4)};
   /* @TODO: add yellow color to ASC-UI and replace it here */
   background-color: #f6c948;
