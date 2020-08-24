6772108ae4e505e821f67ac4e6681c100001b422
Sven Jens <sven@sven-jens.nl>
2020-08-24 16:40:06 +0200

    forgot zonnepanelen intro

--- apps/client/src/intros/ZonnepanelenIntro.js
+++ apps/client/src/intros/ZonnepanelenIntro.js
@@ -1,31 +1,24 @@
-import { Paragraph } from "@datapunt/asc-ui";
+import { OrderedList, Paragraph } from "@datapunt/asc-ui";
 import React from "react";
 
-import { ListItem, OrderedList } from "../atoms";
+import { ListItem } from "../atoms";
 
 export default () => (
   <>
-    <Paragraph>
-      Met de vergunningcheck kunt u zien of u voor het plaatsen van zonnepanelen
-      of een zonneboiler één of meer omgevingsvergunningen nodig hebt. Er zijn 2
-      soorten vergunningen:
+    <Paragraph gutterBottom={12}>
+      Met de vergunningcheck kunt u zien wanneer u een omgevingsvergunning nodig
+      hebt. U kunt een vergunning nodig hebben voor:
     </Paragraph>
     <OrderedList>
-      <ListItem>
-        Een vergunning voor het wijzigen van een monument: ‘wijzigen monument’.
-      </ListItem>
-      <ListItem>
-        Een vergunning voor het plaatsen van de zonnepanelen of zonneboiler
-        zelf: ‘zonnepanelen of zonneboiler plaatsen’.
-      </ListItem>
+      <ListItem>het wijzigen van een monument.</ListItem>
+      <ListItem>het plaatsen van de zonnepanelen of zonneboiler zelf.</ListItem>
     </OrderedList>
 
     <Paragraph>
-      Het hangt af van uw antwoorden en situatie of u een vergunning nodig hebt.
-      U kunt een antwoord wijzigen. Zo kunt u zien op welke manier u misschien
-      toch geen vergunning nodig hebt.
+      Uw situatie en uw antwoorden bepalen of u een omgevingsvergunning nodig
+      hebt. U kunt een antwoord wijzigen. Zo kunt u zien op welke manier u
+      misschien toch geen vergunning nodig hebt.
     </Paragraph>
-
     <Paragraph>
       U kunt deze vergunningcheck gebruiken als u nieuwe zonnepanelen of een
       nieuwe zonneboiler plaatst of als u een bestaande vernieuwt.
