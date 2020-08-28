bbb3f4b6240e1a1c15aa7837741e60414abd6e74
André Lambert <afj.lambert@gmail.com>
2020-08-28 11:18:15 +0200

    Refactor hooks

--- apps/client/src/hoc/withChecker.tsx
+++ apps/client/src/hoc/withChecker.tsx
@@ -10,127 +10,131 @@ import LoadingPage from "../pages/LoadingPage";
 import RedirectPage from "../pages/RedirectPage";
 import getChecker from "../sttr_client";
 
-export default (Component: any) => {
-  return () => {
-    const sessionContext = useContext(SessionContext) as SessionDataType;
-    const checkerContext = useContext(CheckerContext);
-    const [checker, setChecker] = useState(checkerContext.checker);
-    const [error, setError] = useState();
-    const { slug } = useParams();
+export default (Component: any) => () => {
+  const sessionContext = useContext(SessionContext) as SessionDataType;
+  const checkerContext = useContext(CheckerContext);
+  const [checker, setChecker] = useState(checkerContext.checker);
+  const [error, setError] = useState();
+  const { slug } = useParams();
 
-    const address = sessionContext[slug]?.address;
-    const topic = topics.find((t) => t.slug === slug);
+  const topic = topics.find((t) => t.slug === slug);
 
-    useEffect(() => {
-      // if the topic is not found (dynamic sttr-checker) or the topic is found and has an sttr flow
-      if (!topic || topic.hasSTTR) {
-        initChecker();
-      }
-      if (!sessionContext[slug]) {
-        sessionContext.setSessionData([
-          slug,
-          { activeComponents: ["locationInput"], finishedComponents: [] },
-        ]);
-      }
-    });
-
-    const initChecker = async () => {
-      if (!checker && !error) {
-        try {
-          const topicsRequest = await fetch(
-            `${window.location.origin}/topics.json`
-          );
-          const topicsJson = await topicsRequest.json();
-
-          const topicConfig = topicsJson
-            .flat()
-            .find((topic: TopicOutputType) => topic.slug === slug);
-
-          const topicRequest = await fetch(
-            `${window.location.origin}/${topicConfig.path}.json`
-          );
-
-          const newChecker = getChecker(await topicRequest.json());
-
-          // Find if we have missing data needs
-          if (address) {
-            newChecker.autofill(autofillResolvers, { address });
-          }
-
-          const unfulfilledDataNeed = newChecker.getAutofillDataNeeds(
-            autofillMap,
-            true
-          )[0];
-
-          // TODO: Add comment about the next if
-          if (sessionContext[slug]?.answers && !unfulfilledDataNeed) {
-            newChecker.setQuestionAnswers(sessionContext[slug].answers);
-          }
-          // Store the entire `sttr-checker` in React Context
-          checkerContext.checker = newChecker;
-          setChecker(newChecker);
-        } catch (e) {
-          setError(e);
-        }
-      }
-    };
-
-    const resetChecker = () => {
-      setChecker(null);
-      initChecker();
-    };
-
-    // In case of error return the ErrorPage, eg. json not found
-    if (error) {
-      console.error(error);
-      return <ErrorPage {...{ error }} />;
+  useEffect(() => {
+    // Default settings to be able to open the CheckerPage
+    if (!sessionContext[slug]) {
+      sessionContext.setSessionData([
+        slug,
+        { activeComponents: ["locationInput"], finishedComponents: [] },
+      ]);
     }
+  });
+  useEffect(() => {
+    initChecker();
+  });
 
-    // Setup the olo + redirectToOlo flow
-    if (topic) {
-      // Redirect to olo if needed
-      if (topic.redirectToOlo) {
-        return <RedirectPage {...{ topic }} />;
-      }
-
-      // Use olo-flow if it's not an sttr-topic
-      if (!topic.hasSTTR) {
-        return (
-          <Component {...{ topic, checker: null /* XXX */, resetChecker }} />
+  const initChecker = async () => {
+    // if the topic is not found (dynamic sttr-checker) or the topic is found and has an sttr flow
+    if (!checker && !error && (!topic || topic.hasSTTR)) {
+      try {
+        const topicsRequest = await fetch(
+          `${window.location.origin}/topics.json`
         );
+        const topicsJson = await topicsRequest.json();
+
+        const topicConfig = topicsJson
+          .flat()
+          .find((topic: TopicOutputType) => topic.slug === slug);
+
+        const topicRequest = await fetch(
+          `${window.location.origin}/${topicConfig.path}`
+        );
+
+        const newChecker = getChecker(await topicRequest.json());
+        const address = sessionContext[slug]?.address;
+
+        // Find if we have missing data needs
+        if (address) {
+          newChecker.autofill(autofillResolvers, { address });
+        }
+
+        const unfulfilledDataNeed = newChecker.getAutofillDataNeeds(
+          autofillMap,
+          true
+        )[0];
+
+        // TODO: Add comment about the next if
+        if (sessionContext[slug]?.answers && !unfulfilledDataNeed) {
+          newChecker.setQuestionAnswers(sessionContext[slug].answers);
+        }
+        // Store the entire `sttr-checker` in React Context
+        checkerContext.checker = newChecker;
+        setChecker(newChecker);
+      } catch (e) {
+        setError(e);
       }
     }
-
-    // if checker is not initialized then we're still waiting for the json
-    if (!checker) {
-      return <LoadingPage />;
-    }
-
-    // We have the configured checker
-    if (topic) {
-      // This is a configured topic, the most common type
-      checkerContext.topic = topic;
-      return <Component {...{ topic, checker, resetChecker }} />;
-    }
-
-    /**
-     * Fallback scenario;
-     * We don't have a configured topic for the current slug.
-     * Setup a 'fake' topic configuration and render the page.
-     */
-    const name =
-      checker.permits.length === 1
-        ? checker.permits[0].name
-        : "Unconfigured checker with multiple permits";
-    const dynamicTopic: Topic = {
-      slug,
-      name,
-      hasSTTR: true,
-      text: {
-        heading: name,
-      },
-    };
-    checkerContext.topic = dynamicTopic;
-    return <Component {...{ topic: dynamicTopic, checker, resetChecker }} />;
   };
+
+  const resetChecker = () => {
+    setChecker(null);
+    initChecker();
+  };
+
+  if (checkerContext.topic && checkerContext.topic.slug !== slug) {
+    // slug changed reset the topic and checker on sessioncontext
+    checkerContext.topic = null;
+    checkerContext.checker = null;
+    return resetChecker();
+  }
+
+  // In case of error return the ErrorPage, eg. json not found
+  if (error) {
+    console.error(error);
+    return <ErrorPage {...{ error }} />;
+  }
+
+  // Redirect to olo if needed
+  if (topic?.redirectToOlo) {
+    return <RedirectPage {...{ topic }} />;
+  }
+
+  // Use olo-flow if it's not an sttr-topic
+  if (topic?.hasSTTR === false) {
+    return <Component {...{ topic }} />;
+  }
+
+  // if checker is not initialized then we're still waiting for the json
+  if (!checker) {
+    return <LoadingPage />;
+  }
+
+  // We have the configured checker
+  if (topic) {
+    // This is a configured topic, the most common type
+    checkerContext.topic = topic;
+    return <Component {...{ topic, checker, resetChecker }} />;
+  }
+
+  /**
+   * Fallback scenario;
+   * We don't have a configured topic for the current slug.
+   * Setup a 'fake' topic configuration and render the page.
+   */
+  const name =
+    Math.random() +
+    (checker.permits.length === 1
+      ? checker.permits[0].name
+      : "Unconfigured checker with multiple permits");
+
+  const dynamicTopic: Topic = {
+    slug,
+    name,
+    hasSTTR: true,
+    text: {
+      heading: name,
+    },
+  };
+
+  checkerContext.topic = dynamicTopic;
+  return <Component {...{ topic: dynamicTopic, checker, resetChecker }} />;
 };
