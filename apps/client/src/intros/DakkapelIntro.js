cc1d65eb2e461e897e99fd969e18dabb4f933ac2
André Lambert <afj.lambert@gmail.com>
2020-08-28 11:48:26 +0200

    Add dakkapel intro poc

--- apps/client/src/intros/DakkapelIntro.js
+++ apps/client/src/intros/DakkapelIntro.js
@@ -1,19 +1,45 @@
+import { Heading, Paragraph } from "@datapunt/asc-ui";
 import React from "react";
 
-import { Intro } from "./shared";
+import { List, ListItem } from "../atoms";
+import ContactSentence from "../components/ContactSentence";
+import { eventNames } from "../config/matomo";
+import { IntroHeading, IntroSituationDescription } from "./shared";
 
 export default () => (
-  <Intro
-    usableForBullets={[
-      "het wijzigen van een monument.",
-      "het plaatsen van de dakkapel zelf.",
-    ]}
-    usableForText="U kunt deze vergunningcheck gebruiken als u een nieuwe dakkapel plaatst of
-        als u een bestaande vernieuwt."
-    exceptions={[
-      "U gaat de dakkapel plaatsen op een woonwagen, een tijdelijk gebouw of een vakantiehuis.",
-      "Het aantal woningen verandert.",
-      "Het deel van het gebouw waarin de dakkapel komt, is zonder vergunning gebouwd.",
-    ]}
-  />
+  <>
+    <IntroHeading />
+
+    <List variant="bullet">
+      <ListItem>het wijzigen van een monument.</ListItem>
+      <ListItem>het plaatsen van de dakkapel zelf.</ListItem>
+    </List>
+
+    <IntroSituationDescription />
+
+    <Paragraph>
+      U kunt deze vergunningcheck gebruiken als u een nieuwe dakkapel plaatst of
+      als u een bestaande vernieuwt.
+    </Paragraph>
+
+    <Heading forwardedAs="h4">Uitzonderingen:</Heading>
+    <Paragraph gutterBottom={8}>
+      In de volgende situaties is het niet mogelijk de vergunningcheck te
+      gebruiken:
+    </Paragraph>
+    <List variant="bullet" style={{ marginBottom: 12 }}>
+      <ListItem>
+        U gaat de dakkapel plaatsen op een woonwagen, een tijdelijk gebouw of
+        een vakantiehuis.
+      </ListItem>
+      <ListItem>Het aantal woningen verandert.</ListItem>
+      <ListItem>
+        Het deel van het gebouw waarin de dakkapel komt, is zonder vergunning
+        gebouwd.
+      </ListItem>
+    </List>
+    <Paragraph>
+      <ContactSentence eventName={eventNames.INTRO_EXTRAORDINARY} />
+    </Paragraph>
+  </>
 );
