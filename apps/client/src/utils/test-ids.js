c6b5fefea8c88943319de8d2d4fc9f4dc5c45af9
André Lambert <afj.lambert@gmail.com>
2020-08-28 11:17:40 +0200

    Add test-ids

--- apps/client/src/utils/test-ids.js
+++ apps/client/src/utils/test-ids.js
@@ -1,18 +1,25 @@
-export const LOCATION_FOUND = "location-found";
-
-export const LOCATION_RESULT = "address-form";
-
-export const LOADING_TEXT = "loading-text";
-
-export const FOOTER = "footer";
-
-export const QUESTION_PAGE = "question-form";
-export const QUESTION_ANSWERS = "question-answers";
-
-export const NEXT_BUTTON = "next-button";
-export const PREV_BUTTON = "prev-button";
-
-export const INTRO = "intro";
-
+// Navigation
 export const STEPBYSTEPNAVIGATION = "step-by-step-navigation";
 export const STEPBYSTEPITEM = "step-by-step-item";
+export const NEXT_BUTTON = "next-button";
+export const PREV_BUTTON = "prev-button";
+export const LOADING_TEXT = "loading-text";
+export const PHONE_NUMBER = "phone-number";
+
+// Layout
+export const FOOTER = "footer";
+
+// Intro
+export const INTRO = "intro";
+export const INTRO_USER_INFLUENCE = "intro-user-influence";
+export const INTRO_USABLE_FOR_BULLETS = "intro-usable-for-bullets";
+export const INTRO_USABLE_FOR_TEXT = "intro-usable-for-text";
+export const INTRO_EXCEPTION_BULLETS = "intro-exception-bullets";
+
+// Location
+export const LOCATION_FOUND = "location-found";
+export const LOCATION_RESULT = "address-form";
+
+// Questions
+export const QUESTION_PAGE = "question-form";
+export const QUESTION_ANSWERS = "question-answers";
