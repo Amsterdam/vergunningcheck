5b46160f7e764bd6e737f7e8efccb87d614b468d
André Lambert <afj.lambert@gmail.com>
2020-09-07 12:02:31 +0200

    Move all intro's

--- apps/client/src/intros/ZonnepanelenIntro.js
+++ apps/client/src/intros/ZonnepanelenIntro.js
@@ -1,23 +1,17 @@
-import { Paragraph } from "@datapunt/asc-ui";
 import React from "react";
 
-import { List, ListItem } from "../atoms";
-import { IntroHeading, IntroSituationDescription } from "./shared";
+import { Intro } from "./shared";
 
 export default () => (
-  <>
-    <IntroHeading />
-
-    <List variant="bullet">
-      <ListItem>het wijzigen van een monument.</ListItem>
-      <ListItem>het plaatsen van de zonnepanelen of zonneboiler zelf.</ListItem>
-    </List>
-
-    <IntroSituationDescription />
-
-    <Paragraph>
-      U kunt deze vergunningcheck gebruiken als u nieuwe zonnepanelen of een
-      nieuwe zonneboiler plaatst of als u een bestaande vernieuwt.
-    </Paragraph>
-  </>
+  <Intro
+    // Not sure why we wouldn't want to show the contact information in this intro-page.
+    // It's the only exception.
+    showContactInformation={false}
+    usableForBullets={[
+      "het wijzigen van een monument.",
+      "het plaatsen van de zonnepanelen of zonneboiler zelf.",
+    ]}
+    usableForText={`U kunt deze vergunningcheck gebruiken als u nieuwe zonnepanelen of een
+      nieuwe zonneboiler plaatst of als u een bestaande vernieuwt.`}
+  />
 );
