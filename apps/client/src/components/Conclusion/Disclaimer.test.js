364e2d1cb535fd4eaaea17da5d0614cd1edd2683
Robin Piets <robinpiets@gmail.com>
2020-09-23 14:42:54 +0200

    Added test for Disclaimer

---
+++ apps/client/src/components/Conclusion/Disclaimer.test.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import { DISCLAIMER_TEXT } from "../../utils/test-ids";
+import { cleanup, render } from "../../utils/test-utils";
+import Disclaimer from "./Disclaimer";
+
+afterEach(cleanup);
+
+describe("Disclaimer", () => {
+  it("renders correctly", () => {
+    const { getByText, queryByTestId } = render(<Disclaimer />);
+    // Should be in document
+    expect(queryByTestId(DISCLAIMER_TEXT)).toBeInTheDocument();
+    expect(getByText("Let op")).toBeInTheDocument();
+  });
+});
--- apps/client/src/components/Conclusion/Disclaimer.tsx
+++ apps/client/src/components/Conclusion/Disclaimer.tsx
@@ -1,11 +1,12 @@
 import { Alert, Paragraph } from "@datapunt/asc-ui";
 import React from "react";
 
+import { DISCLAIMER_TEXT } from "../../utils/test-ids";
 import ContactSentence from "../ContactSentence";
 
 const Disclaimer: React.FC = () => (
   <Alert heading="Let op">
-    <Paragraph>
+    <Paragraph data-testid={DISCLAIMER_TEXT}>
       De vergunningcheck is nog in ontwikkeling. Hierdoor kunnen wij nog geen
       zekerheid bieden dat de uitkomst correct is. Ook is de informatie nog niet
       voor iedereen goed te lezen of te beluisteren. Wilt u iets zeker weten of
