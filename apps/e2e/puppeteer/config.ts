75961c71f27c8abc109acf1c5c2a8ccc24c0d552
André Lambert <afj.lambert@gmail.com>
2020-10-08 14:55:20 +0200

    Make tests more configurable and test for new location component

---
+++ apps/e2e/puppeteer/config.ts
@@ -0,0 +1,32 @@
+import { Flow } from "./types";
+
+// export const host = "https://vergunningcheck.amsterdam.nl/test";
+export const host = "https://ux.chappie2.com";
+
+export const DEBUG = false;
+
+export const puppeteerOptions = DEBUG ? {
+  headless: false, slowMo: 150
+} : {};
+
+export const address = {
+  zipCode: '1043ap',
+  houseNumberFull: '30',
+}
+
+export const flows: Flow[] = [
+  {
+    type: 'olo',
+    options: {
+      shouldAlwaysDisplayMonumentStatus: true,
+    },
+    checkers: ["aanbouw-of-uitbouw-maken", "bouwwerk-slopen", "intern-verbouwen"],
+  },
+  {
+    type: 'imtr',
+    options: {
+      shouldAlwaysDisplayMonumentStatus: false,
+    },
+    checkers: ["dakkapel-plaatsen", "dakraam-plaatsen", "kozijnen-plaatsen", "zonnepanelen-of-zonneboiler-plaatsen", "zonwering-of-rolluik-plaatsen"],
+  }
+];
