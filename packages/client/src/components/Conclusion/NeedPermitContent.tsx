8e62afe0e7f6c412e2d6343097ba1bae5876a072
Sven Jens <sven@sven-jens.nl>
2020-10-19 17:56:22 +0200

    updated url key

--- packages/client/src/components/Conclusion/NeedPermitContent.tsx
+++ packages/client/src/components/Conclusion/NeedPermitContent.tsx
@@ -1,7 +1,7 @@
-import { Button, Link, Paragraph } from "@amsterdam/asc-ui";
+import { Button, Paragraph } from "@amsterdam/asc-ui";
 import React from "react";
 
-import { HideForPrint, PrintOnly } from "../../atoms";
+import { HideForPrint } from "../../atoms";
 import ComponentWrapper from "../../atoms/ComponentWrapper";
 import { urls } from "../../config";
 import { actions, eventNames } from "../../config/matomo";
@@ -16,20 +16,11 @@ const NeedPermitContent: React.FC<{ matomoTrackEvent: Function }> = ({
       name: eventNames.HOW_TO_APPLY_FOR_A_PERMIT,
     });
     // Open OLO in new tab/window
-    window.open(urls.HOW_TO_GET_A_PERMIT, "_blank");
+    window.open(urls.HOW_TO_APPLY_FOR_A_PERMIT, "_blank");
   };
 
   return (
     <>
-      <Paragraph gutterBottom={16}>
-        <HideForPrint as="span">bij het landelijk Omgevingsloket.</HideForPrint>
-        <PrintOnly as="span">
-          op{" "}
-          <Link variant="inline" href={urls.OLO_HOME}>
-            www.omgevingsloket.nl
-          </Link>
-        </PrintOnly>
-      </Paragraph>
       <Paragraph>
         U leest meer over hoe u een vergunning aanvraagt, wat de kosten zijn en
         hoe lang het duurt op onze pagina over het aanvragen van een
