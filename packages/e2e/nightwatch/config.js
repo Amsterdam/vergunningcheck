9cb4d8fe361e5a4a6ebe478289fcd0f89938ef18
Robin Piets <robinpiets@gmail.com>
2020-10-20 18:37:40 +0200

    Added e2e test for IMTR flow

--- packages/e2e/nightwatch/config.js
+++ packages/e2e/nightwatch/config.js
@@ -1,6 +1,9 @@
 const config = {
   domain: "http://localhost:3000", // @TODO read from environment variable
   selectors: {
+    conclusionContact: "[data-testid=need-contact]",
+    conclusionNeedPermitButton: "[data-testid=need-permit-button]",
+    introBullets: "[data-testid=intro-usable-for-bullets]",
     locationHouseNumberFull: "input[name=houseNumberFull]",
     locationFound: "[data-testid=location-found]",
     locationPostalCode: "input[name=postalCode]",
@@ -9,6 +12,12 @@ const config = {
     main: "body",
     navButtonPrev: "[data-testid=prev-button]",
     navButtonNext: "[data-testid=next-button]",
+    q1a1: "[data-testid=q1-a1]",
+    q1a2: "[data-testid=q1-a2]",
+    q2a1: "[data-testid=q2-a1]",
+    questionAlert: "[role=alert]",
+    questionEditButton: "[data-testid=edit-button",
+    questionForm: "[data-testid=question-form]",
   },
   olo: {
     selectors: {
