95d92917ff498d0c6562eb1529c28cbe8d85b475
Robin Piets <robinpiets@gmail.com>
2020-10-20 11:29:41 +0200

    Added the `how to apply for permit` URL on print only

--- packages/client/src/components/Conclusion/NeedPermitContent.tsx
+++ packages/client/src/components/Conclusion/NeedPermitContent.tsx
@@ -1,7 +1,7 @@
-import { Button, Paragraph } from "@amsterdam/asc-ui";
+import { Button, Link, Paragraph } from "@amsterdam/asc-ui";
 import React from "react";
 
-import { HideForPrint } from "../../atoms";
+import { HideForPrint, PrintOnly } from "../../atoms";
 import ComponentWrapper from "../../atoms/ComponentWrapper";
 import { urls } from "../../config";
 import { actions, eventNames } from "../../config/matomo";
@@ -25,6 +25,12 @@ const NeedPermitContent: React.FC<{ matomoTrackEvent: Function }> = ({
         hoe lang het duurt op onze pagina over het aanvragen van een
         omgevingsvergunning.
       </Paragraph>
+      <PrintOnly as="span">
+        Zo werkt aanvragen:{" "}
+        <Link variant="inline" href={urls.HOW_TO_APPLY_FOR_A_PERMIT}>
+          {urls.HOW_TO_APPLY_FOR_A_PERMIT}
+        </Link>
+      </PrintOnly>
       <HideForPrint>
         <ComponentWrapper marginBottom={32}>
           <Button
