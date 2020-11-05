bf8453aaefdd24cb6d21ae365fb937c090716546
Robin Piets <robinpiets@gmail.com>
2020-11-05 13:57:15 +0100

    Updated disclaimer text

--- packages/client/src/components/Disclaimer.test.js
+++ packages/client/src/components/Disclaimer.test.js
@@ -4,14 +4,15 @@ import { DISCLAIMER_TEXT } from "../utils/test-ids";
 import { render, screen } from "../utils/test-utils";
 import Disclaimer from "./Disclaimer";
 
-// const text =
-//   "Deze uitkomst is niet definitief. Uit deze vergunningcheck kunt u geen rechten halen.";
+// @TODO: import this text from i18n
+const text =
+  "Deze uitkomst is niet definitief. Uit deze vergunningcheck kunt u geen rechten halen.";
 
 describe("Disclaimer", () => {
   it("renders correctly", () => {
     render(<Disclaimer />);
     // Should be in document
     expect(screen.queryByTestId(DISCLAIMER_TEXT)).toBeInTheDocument();
-    // expect(screen.getByText(text)).toBeInTheDocument();
+    expect(screen.getByText(text)).toBeInTheDocument();
   });
 });
