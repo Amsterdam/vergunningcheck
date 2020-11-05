ec547a1a8fbf671d80adef64aa569356c269e5e8
Robin Piets <robinpiets@gmail.com>
2020-11-05 13:53:42 +0100

    Removed the clicking disclaimer for the ux-test

--- packages/client/src/components/Disclaimer.tsx
+++ packages/client/src/components/Disclaimer.tsx
@@ -1,25 +1,19 @@
 import { CompactThemeProvider, Paragraph } from "@amsterdam/asc-ui";
-import React, { useState } from "react";
+import React from "react";
 
 import { DISCLAIMER_TEXT } from "../utils/test-ids";
 
 const Disclaimer: React.FC = () => {
-  const [change, setChange] = useState(false);
-
   return (
     <CompactThemeProvider>
-      <div onClick={() => setChange(!change)} style={{ userSelect: "none" }}>
-        <Paragraph
-          data-testid={DISCLAIMER_TEXT}
-          forwardedAs="em"
-          gutterBottom={0}
-        >
-          {change
-            ? `Deze uitkomst is niet definitief. Uit deze vergunningcheck kunt u geen
-          rechten halen.`
-            : `De gemeente Amsterdam doet er alles aan om u juiste informatie te geven. Maar u kunt aan deze uitkomst geen rechten ontlenen. Als u een aanvraag doet, kunt u zekerheid krijgen. `}
-        </Paragraph>
-      </div>
+      <Paragraph
+        data-testid={DISCLAIMER_TEXT}
+        forwardedAs="em"
+        gutterBottom={0}
+      >
+        Deze uitkomst is niet definitief. Uit deze vergunningcheck kunt u geen
+        rechten halen.
+      </Paragraph>
     </CompactThemeProvider>
   );
 };
