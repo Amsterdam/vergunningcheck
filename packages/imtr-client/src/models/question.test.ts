ea6321182027407a554c0ff9e76a3a10826a70b1
André Lambert <afj.lambert@gmail.com>
2020-11-16 19:56:07 +0100

    Add more tests

--- packages/imtr-client/src/models/question.test.ts
+++ packages/imtr-client/src/models/question.test.ts
@@ -1,3 +1,4 @@
+import { addQuotes } from "../utils";
 import Question from "./question";
 
 const getQuestion = ({ ...overrides }: any) =>
@@ -13,44 +14,52 @@ const getQuestion = ({ ...overrides }: any) =>
 describe("Question", () => {
   it("initializes", () => {
     const { id, type, text, description } = getQuestion({});
-    expect(id).toBe("aaa");
-    expect(type).toBe("boolean");
-    expect(text).toBe("Are you ok?");
-    expect(description).toBe("Describe how you are _feeling_.");
+    expect(id).toEqual("aaa");
+    expect(type).toEqual("boolean");
+    expect(text).toEqual("Are you ok?");
+    expect(description).toEqual("Describe how you are _feeling_.");
   });
 
-  it("can be answered", () => {
+  it("can be answered with strings", () => {
     const question = getQuestion({});
     question.setAnswer(true);
-    expect(question.answer).toBe(true);
+    expect(question.answer).toEqual(true);
     question.setAnswer(false);
-    expect(question.answer).toBe(false);
+    expect(question.answer).toEqual(false);
     expect(() => question.setAnswer("goooo")).toThrow(
       "value for setAnswer must be of type boolean, got 'goooo'"
     );
   });
 
   it("supports collection", () => {
-    expect(() => getQuestion({ collection: true, type: "boolean" })).toThrow(
+    const options = ["a", "b"];
+    const quotedOptions = options.map(addQuotes);
+    expect(() =>
+      getQuestion({ collection: true, options, type: "boolean" })
+    ).toThrow(
       "Cannot construct question with collection: true but not type string (got 'boolean')"
     );
 
-    expect(() => getQuestion({ collection: true })).toThrow(
+    expect(() => getQuestion({ collection: true, type: "string" })).toThrow(
       "Cannot construct question with collection: true but no options provided"
     );
 
-    const question = getQuestion({ collection: true, options: ["a", "b"] });
+    const question = getQuestion({
+      collection: true,
+      type: "string",
+      options,
+    });
     expect(() => question.setAnswer("goooo")).toThrow(
       "value for setAnswer must be of type string[] for collections, got 'goooo'"
     );
 
-    question.setAnswer(["a", "b"]);
-    expect(question.answer).toBe(["a", "b"]);
-    question.setAnswer(["a"]);
-    expect(question.answer).toBe(["a"]);
-
-    expect(() => question.setAnswer(["c"])).toThrow(
-      "value 'c' in setAnswer is not in options-list ['a', 'b']"
+    expect(() => question.setAnswer(['"c"'])).toThrow(
+      'value \'"c"\' in setAnswer is not in options-list \'"a","b"\''
     );
+
+    question.setAnswer(quotedOptions);
+    expect(question.answer).toEqual(quotedOptions);
+    question.setAnswer(['"a"']);
+    expect(question.answer).toEqual(['"a"']);
   });
 });
