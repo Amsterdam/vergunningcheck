6c070e877d1b182bb0408b8f94dbae8441acc3fd
Robin Piets <robinpiets@gmail.com>
2020-12-03 13:11:15 +0100

    Remove error when there's no slug

--- packages/client/src/hooks/useSlug.ts
+++ packages/client/src/hooks/useSlug.ts
@@ -5,8 +5,6 @@ import { getSlugFromPathname } from "../utils";
 export default (): string => {
   const { pathname } = useLocation();
   const slug = getSlugFromPathname(pathname);
-  if (!slug && pathname !== "/") {
-    throw new Error(`useSlug: slug not found (${pathname})`);
-  }
+
   return slug;
 };
