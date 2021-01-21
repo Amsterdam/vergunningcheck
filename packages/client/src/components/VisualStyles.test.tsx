f6bac41f704376c2859208642904f9e4d38f0316
Sven Jens <sven@sven-jens.nl>
2021-01-21 15:24:12 +0100

    added on error test

--- packages/client/src/components/VisualStyles.test.tsx
+++ packages/client/src/components/VisualStyles.test.tsx
@@ -1,56 +1,61 @@
 import "jest-styled-components";
 
+import { captureException } from "@sentry/browser";
 import React from "react";
 
 import { FIGCAPTION, FIGURE, IMG } from "../utils/test-ids";
-import { render, screen } from "../utils/test-utils";
-import { Figure, Img } from "./VisualStyles";
+import { fireEvent, render, screen } from "../utils/test-utils";
+import Visual from "./Visual";
 
-it("Figure renders correctly without props", () => {
-  render(<Figure data-testid={FIGURE} />);
+jest.mock("@sentry/browser");
 
-  expect(screen.queryByTestId(FIGURE)).toBeInTheDocument();
-  expect(screen.queryByTestId(FIGURE)).toHaveStyleRule("height", "0");
+describe("Visual", () => {
+  it("renders correctly without props", () => {
+    render(<Visual src="folder/image.jpg" />);
 
-  expect(screen.queryByTestId(IMG)).not.toBeInTheDocument();
-  expect(screen.queryByTestId(FIGCAPTION)).not.toBeInTheDocument();
-});
+    expect(screen.queryByTestId(FIGURE)).toBeInTheDocument();
+    expect(screen.queryByTestId(FIGURE)).toHaveStyleRule("height", "0");
 
-it("Figure renders correctly with props", () => {
-  render(<Figure data-testid={FIGURE} errored loaded />);
+    expect(screen.queryByTestId(IMG)).toHaveStyleRule(
+      "border",
+      "1px solid transparent"
+    );
+    expect(screen.queryByTestId(IMG)?.getAttribute("src")).toBe(
+      "folder/image.jpg"
+    );
 
-  expect(screen.queryByTestId(FIGURE)).toBeInTheDocument();
-  expect(screen.queryByTestId(FIGURE)).not.toHaveStyleRule("height", "0");
-  expect(screen.queryByTestId(FIGURE)).toHaveStyleRule("min-height", "1px");
+    expect(screen.queryByTestId(FIGCAPTION)).not.toBeInTheDocument();
+  });
 
-  expect(screen.queryByTestId(IMG)).not.toBeInTheDocument();
-  expect(screen.queryByTestId(FIGCAPTION)).not.toBeInTheDocument();
-});
+  it("renders correctly with props", () => {
+    render(<Visual alt="alt text" src="folder/image.jpg" title="title" />);
 
-it("Img renders shouldn't render", () => {
-  render(<Img data-testid={IMG} />);
+    expect(screen.queryByAltText("alt text")).toBeInTheDocument();
+    expect(screen.queryByText("title")).toBeInTheDocument();
+    expect(screen.queryByTestId(FIGCAPTION)).toBeInTheDocument();
+  });
 
-  expect(screen.queryByTestId(IMG)).toBeInTheDocument();
-  expect(screen.queryByTestId(IMG)).toHaveStyleRule(
-    "border",
-    "1px solid transparent"
-  );
+  it("renders correctly with on load", () => {
+    const { container } = render(
+      <Visual alt="alt text" src="folder/image.jpg" title="title" />
+    );
 
-  expect(screen.queryByTestId(IMG)).not.toHaveAttribute("src");
-  expect(screen.queryByTestId(FIGURE)).not.toBeInTheDocument();
-  expect(screen.queryByTestId(FIGCAPTION)).not.toBeInTheDocument();
-});
+    const img = container.querySelector("img") as HTMLImageElement;
+    expect(img).not.toBeNull();
+    fireEvent(img, new Event("load"));
+    expect(img).toHaveStyleRule("border", "1px solid #767676");
+    expect(img).toHaveStyleRule("padding", "0");
+  });
 
-it("Img renders correctly with props", () => {
-  render(<Img data-testid={IMG} errored loaded src="folder/image.jpg" />);
+  it("renders correctly with on error", () => {
+    const { container } = render(
+      <Visual alt="alt text" src="folder/image.jpg" title="title" />
+    );
 
-  expect(screen.queryByTestId(IMG)).toBeInTheDocument();
-  expect(screen.queryByTestId(IMG)).toHaveAttribute("src");
-  expect(screen.queryByTestId(IMG)).toHaveStyleRule(
-    "border",
-    "1px solid #767676"
-  );
-
-  expect(screen.queryByTestId(FIGURE)).not.toBeInTheDocument();
-  expect(screen.queryByTestId(FIGCAPTION)).not.toBeInTheDocument();
+    const img = container.querySelector("img") as HTMLImageElement;
+    expect(img).not.toBeNull();
+    fireEvent(img, new Event("error"));
+    expect(captureException).toHaveBeenCalledWith("folder/image.jpg not found");
+    expect(img).toHaveStyleRule("padding", "33px");
+  });
 });
