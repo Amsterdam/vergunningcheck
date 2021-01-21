bd60ab76d9e92228427b173358ac43a6ef763a77
Sven Jens <sven@sven-jens.nl>
2021-01-21 10:12:57 +0100

    updated translations

--- packages/client/src/pages/olo/OloRedirectPage.test.tsx
+++ packages/client/src/pages/olo/OloRedirectPage.test.tsx
@@ -1,9 +1,9 @@
 import "@testing-library/jest-dom/extend-expect";
 
-import i18n from "i18next";
 import React from "react";
 
 import { urls } from "../../config";
+import nl from "../../i18n/nl";
 import { render, screen } from "../../utils/test-utils";
 import OloRedirectPage from "./OloRedirectPage";
 
@@ -23,13 +23,13 @@ describe("OloRedirectPage", () => {
   it("should render and redirect correctly", async () => {
     render(<OloRedirectPage />);
 
-    const headingText = i18n.t("oloRedirectPage.heading");
+    const headingText = nl.translation.oloRedirectPage.heading;
     await screen.findByText(headingText, {
       exact: false,
     });
-    const bodyText = i18n.t("oloRedirectPage.paragraph");
+    const bodyText = nl.translation.oloRedirectPage.paragraph;
     await screen.findByText(bodyText, { exact: false });
-    const linkText = i18n.t("oloRedirectPage.link");
+    const linkText = nl.translation.oloRedirectPage.link;
     await screen.findByText(linkText, { exact: false });
 
     // schedule redirect
