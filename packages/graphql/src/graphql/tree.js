693e0868e122c928bde0dae4393c41a6c2c8a20b
André Lambert <afj.lambert@gmail.com>
2021-01-27 17:16:33 +0100

    WIP list query

--- packages/graphql/src/graphql/tree.js
+++ packages/graphql/src/graphql/tree.js
@@ -1,47 +1,52 @@
 const { gql } = require("../util");
 
-/**
- * TODO extend GraphQL schema with geolocation.
- *
- * ```
- * type Geo {
- *   type: String
- *   features {
- *     type
- *     geometry {
- *       type
- *       coordinates
- *     }
- *     properties {
- *       id
- *     }
- *   }
- * }
- *
- * type Tree implements Node {
- *   meta: {
- *     monument: MonumentTree!
- *   }
- *   geo: Geo
- * }
- * ```
- */
-
 const typeDefs = gql`
   type Tree implements Node {
     id: ID!
+    meta: TreeMeta!
+    geo: GeoJSONPoint!
+  }
+
+  type TreeMeta {
+    isProtected: Boolean!
   }
 
   extend type Query {
     tree(id: ID!): Tree
-    trees: [Tree!]!
+    trees(search: GeoSearchInput): [Tree!]!
   }
 `;
 
 const resolvers = {
   Query: {
-    tree: (_, args) => args,
-    trees: (_, __, { loaders }) => loaders.tree.fetch(),
+    tree: (_, args) => ({
+      meta: {},
+      ...args,
+    }),
+    trees: async (_, { search }, { loaders }) => {
+      return (await loaders.tree.fetch(search)).map((data) => ({
+        meta: {},
+        geo: {
+          type: "POINT",
+          features: [
+            {
+              type: "POINT",
+              crs: {
+                type: "NAME",
+                properties: {
+                  name: "the name",
+                },
+              },
+              bbox: [2.1],
+            },
+          ],
+        },
+        ...data,
+      }));
+    },
+  },
+  TreeMeta: {
+    isProtected: () => Math.random() < 0.5, // rand bool
   },
 };
 
--- packages/graphql/src/loaders/tree/index.js
+++ packages/graphql/src/loaders/tree/index.js
@@ -3,13 +3,91 @@ const getPool = require("../../database");
 // TODO: consider moving this file to a different folder as it is not related to
 // the rest api loaders. Also the setup is different and it uses no caching.
 
+/**
+ * `SELECT *, ST_AsGeoJSON(ST_Transform(geometrie, 4326)) as geo FROM grn_vegetatieobject
+        WHERE geometrie && ST_MakeEnvelope (
+          103082.0, 486567.0, -- bounding
+          103683.9, 486868.9, -- box limits
+          4326
+        )
+        LIMIT 50`
+
+
+        `SELECT *, ST_AsGeoJSON(ST_FlipCoordinates(ST_Transform(geometrie, ${sridLatLon}))) as geo
+       FROM grn_vegetatieobject
+       WHERE geometrie && ST_Transform(ST_MakeEnvelope(
+         ${xmin}, ${ymin}, -- bounding
+         ${xmax}, ${ymax}, -- box limits
+         ${sridLatLon}
+       ), ${sridRD})
+       LIMIT 50`
+ */
 const loader = {
-  reducer: (o) => ({
-    id: o.bk_grn_vegetatieobject,
-  }),
-  fetch: async () => {
+  reducer: (o) => {
+    console.log(o);
+    return {
+      id: o.bk_grn_vegetatieobject,
+      // o.tijdstipregistratie: '20120802073221',
+      // o.lv_publicatiedatum: '20151229035805',
+      // o.bronhouder: 'G0394',
+      // o.relatievehoogteligging: '0',
+      // o.plus_type: 'boom',
+      geo: {
+        ...JSON.parse(o.geo),
+        // crs: {
+        //   type: "NAME",
+        //   properties: {
+        //     name: "locatie ofzo?",
+        //   },
+        // },
+      },
+    };
+  },
+  fetch: async (query) => {
+    const geojson = JSON.stringify(query);
     const pool = await getPool();
-    const res = await pool.query("SELECT * FROM grn_vegetatieobject LIMIT 10");
+    // ST_Transform
+    const sridRD = 28992;
+    const sridLatLon = 4326;
+
+    // const lonMax = 4.905888; // rechts
+    // const latMax = 52.370821; // boven
+    // const lonMin = 4.904454; // links
+    // const latMin = 52.370305; // onder
+
+    // const xmin = lonMin;
+    // const ymin = latMin;
+    // const xmax = lonMax;
+    // const ymax = latMax;
+
+    // const geojsonMain = {
+    //   type: "Polygon",
+    //   coordinates: [
+    //     [
+    //       [xmin, ymin], // linksonder
+    //       [xmax, ymin], // rechtsonder
+    //       [xmax, ymax], // rechtsboven
+    //       [xmin, ymax], // linksboven
+    //     ],
+    //   ],
+    // }
+    // const geojsonExtended = {
+    //   type: "Feature",
+    //   geometry: {
+    //     ...geojsonMain
+    //   },
+    // }
+
+    // const geojson =
+    //   '{"type":"Polygon","coordinates":[[[4.904454,52.370305],[4.905888,52.370305],[4.905888,52.370821],[4.904454,52.370821]]]}';
+    // // const geojson = JSON.stringify();
+    // console.log(geojson);
+    const res = await pool.query(
+      `SELECT *, ST_AsGeoJSON(ST_FlipCoordinates(ST_Transform(geometrie, ${sridLatLon}))) as geo
+       FROM grn_vegetatieobject
+       WHERE geometrie && ST_Transform(ST_SetSRID(ST_GeomFromGeoJSON('${geojson}'), ${sridLatLon}), ${sridRD})
+       LIMIT 50`
+    );
     return res.rows.map(loader.reducer);
   },
 };
