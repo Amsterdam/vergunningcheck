fc3d40dc7ec8f27aea56db2a5e5c8724b37b24f3
Robin Piets <robinpiets@gmail.com>
2021-02-01 18:38:27 +0100

    Updated tests

--- packages/imtr/src/util.test.js
+++ packages/imtr/src/util.test.js
@@ -1,26 +1,30 @@
 import { assert } from "./deps.ts";
 import { validateDmnText } from "./util.ts";
 
-Deno.test("validateDmnText 1", async () => {
+Deno.test("validateDmnText with test string", async () => {
   assert(validateDmnText("test") === "test");
 });
 
-Deno.test("validateDmnText 2", async () => {
+Deno.test("validateDmnText with undefined", async () => {
   assert(validateDmnText(undefined) === undefined);
 });
 
-Deno.test("validateDmnText 3", async () => {
+Deno.test("validateDmnText without any value", async () => {
+  assert(validateDmnText() === undefined);
+});
+
+Deno.test("validateDmnText true", async () => {
   assert(validateDmnText(true) === true);
 });
 
-Deno.test("validateDmnText 4", async () => {
+Deno.test("validateDmnText false", async () => {
   assert(validateDmnText(false) === false);
 });
 
-Deno.test("validateDmnText 5", async () => {
+Deno.test("validateDmnText expected result", async () => {
   assert(validateDmnText("-") === "-");
 });
 
-Deno.test("validateDmnText 6", async () => {
-  assert(validateDmnText("-") === "-");
+Deno.test("validateDmnText possible bug in api", async () => {
+  assert(validateDmnText("not(null)") === "-");
 });
