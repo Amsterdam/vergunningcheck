d8b14cc17d672178df46c4b3e1dc48738c365b56
Sven Jens <sven@sven-jens.nl>
2021-02-16 21:32:09 +0100

    git added more selenium

---
+++ packages/e2e/selenium.js
@@ -0,0 +1,54 @@
+const webdriver = require('selenium-webdriver');
+
+const userName = "sjens1";
+const accessKey = "3T6jZ9N4pYf9qjzoqKxU"
+const browserstackURL = `https://${userName}:${accessKey}@hub-cloud.browserstack.com/wd/hub`;
+
+// Input capabilities
+const safari = {
+  'os' : 'OS X',
+  'os_version' : 'High Sierra',
+  'browserName' : 'Safari',
+  'browser_version' : '11.1',
+  'name' : "sjens1's First Test"
+}
+
+const ie = {
+  'os' : 'Windows',
+  'os_version' : '10',
+  'browserName' : 'IE',
+  'browser_version' : '11.0',
+  'name' : "sjens1's First Test"
+}
+
+const driverSafari = new webdriver.Builder().
+usingServer(browserstackURL).
+withCapabilities(safari).
+build();
+
+const driverIe = new webdriver.Builder().
+usingServer(browserstackURL).
+withCapabilities(ie).
+build();
+
+searchTest(driverSafari);
+searchTest(driverIe);
+
+function searchTest(driver) {
+  try {
+    driver.get('http://www.google.com').then(function () {
+      driver.findElement(webdriver.By.name('sq')).sendKeys('BrowserStack').then(function () {
+        driver.getTitle().then(function (title) {
+          console.log(title);
+          driver.quit();
+        });
+      });
+    });
+  } catch (e) {
+    console.log(e);
+  } finally {
+    if(driver) {
+      driver.quit();
+    }
+  }
+}
