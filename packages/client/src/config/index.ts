86d0ef4af3134602127ae18df37678295081f3f0
Robin Piets <robinpiets@gmail.com>
2021-02-16 11:01:13 +0100

    Updated readability of config and documentation (#804)
    * Removed comment
    
    * Updated readibilty of config and documentation
    
    * Removed comment

--- packages/client/src/config/index.ts
+++ packages/client/src/config/index.ts
@@ -1,3 +1,6 @@
+import Topic from "../models/topic";
+import { TopicType } from "../types";
+
 /**
  * Merge the different topic types
  *
@@ -7,44 +10,6 @@
  * text: This is part that holds specific texts for each permit-checker
  * type: See {TopicType} for the different types of Topic
  */
-
-import Topic from "../models/topic";
-import { TopicType } from "../types";
-
-type OloUrlProps = {
-  houseNumber: number;
-  houseNumberFull: string;
-  postalCode: string;
-};
-
-export const oloHome: string = "https://www.omgevingsloket.nl/";
-
-export const isProduction: boolean =
-  "vergunningcheck.amsterdam.nl" === window.location.hostname;
-
-export const urls = {
-  DEMOLITION_PERMIT_PAGE:
-    "https://www.amsterdam.nl/veelgevraagd/?caseid=%7BAEA35C69-4DAD-483E-8AA1-C068D88B792C%7D",
-  GENERAL_PERMIT_PAGE:
-    "https://www.amsterdam.nl/veelgevraagd/?productid=%7B215DE049-EFA3-492D-A4B1-EDFF40E0BC51%7D",
-  OLO_HOME: oloHome,
-  OLO_INTRO: `${oloHome}Particulier/particulier/home?init=true`,
-  OLO_LOCATION: `${oloHome}Particulier/particulier/home/checken/LocatieWerkzaamheden`,
-  VIEW_ZONING_PLAN:
-    "https://www.amsterdam.nl/veelgevraagd/?productid=%7bC25A69DB-3548-4E12-97BB-DB71318EDFB2%7d",
-};
-
-export const generateOloUrl = ({
-  houseNumber,
-  houseNumberFull,
-  postalCode,
-}: OloUrlProps) => {
-  // Get correct suffix
-  const suffix = houseNumberFull.replace(houseNumber.toString(), "").trim();
-  // Redirect user to OLO with all parameters
-  return `${urls.OLO_LOCATION}?param=postcodecheck&facet_locatie_postcode=${postalCode}&facet_locatie_huisnummer=${houseNumber}&facet_locatie_huisnummertoevoeging=${suffix}`;
-};
-
 export const topics: Topic[] = [
   {
     intro: "DakkapelIntro",
@@ -152,3 +117,39 @@ export const topics: Topic[] = [
     type: TopicType.PERMIT_FORM,
   },
 ].map((t) => new Topic(t));
+
+// Is the client running on production?
+export const isProduction: boolean =
+  "vergunningcheck.amsterdam.nl" === window.location.hostname;
+
+// URLS used throughout the client
+export const oloHome: string = "https://www.omgevingsloket.nl/";
+
+export const urls = {
+  DEMOLITION_PERMIT_PAGE:
+    "https://www.amsterdam.nl/veelgevraagd/?caseid=%7BAEA35C69-4DAD-483E-8AA1-C068D88B792C%7D",
+  GENERAL_PERMIT_PAGE:
+    "https://www.amsterdam.nl/veelgevraagd/?productid=%7B215DE049-EFA3-492D-A4B1-EDFF40E0BC51%7D",
+  OLO_HOME: oloHome,
+  OLO_INTRO: `${oloHome}Particulier/particulier/home?init=true`,
+  OLO_LOCATION: `${oloHome}Particulier/particulier/home/checken/LocatieWerkzaamheden`,
+  VIEW_ZONING_PLAN:
+    "https://www.amsterdam.nl/veelgevraagd/?productid=%7bC25A69DB-3548-4E12-97BB-DB71318EDFB2%7d",
+};
+
+type OloUrlProps = {
+  houseNumber: number;
+  houseNumberFull: string;
+  postalCode: string;
+};
+
+export const generateOloUrl = ({
+  houseNumber,
+  houseNumberFull,
+  postalCode,
+}: OloUrlProps) => {
+  // Get correct suffix
+  const suffix = houseNumberFull.replace(houseNumber.toString(), "").trim();
+  // Redirect user to OLO with all parameters
+  return `${urls.OLO_LOCATION}?param=postcodecheck&facet_locatie_postcode=${postalCode}&facet_locatie_huisnummer=${houseNumber}&facet_locatie_huisnummertoevoeging=${suffix}`;
+};
