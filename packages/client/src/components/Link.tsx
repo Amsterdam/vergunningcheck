a5543e101e187322875e0274ca7feef52c857bae
Robin Piets <robinpiets@gmail.com>
2021-02-17 12:36:26 +0100

    Improved Link props

--- packages/client/src/components/Link.tsx
+++ packages/client/src/components/Link.tsx
@@ -1,20 +1,12 @@
 import { Link as StyledComponentLink } from "@amsterdam/asc-ui";
-import React, { ReactNode } from "react";
+import React, { ComponentProps } from "react";
 
 import { actions } from "../config/matomo";
 import { useTracking } from "../hooks";
 
 type LinkProps = {
   action?: string;
-  children: ReactNode;
-  darkBackground?: boolean;
-  href?: string;
   eventName: string;
-  internal?: boolean;
-  inList?: boolean;
-  strong?: boolean;
-  target?: string;
-  variant?: string | null;
 };
 
 const Link = ({
@@ -23,7 +15,7 @@ const Link = ({
   eventName,
   href,
   ...rest
-}: LinkProps) => {
+}: LinkProps & ComponentProps<typeof StyledComponentLink>) => {
   const { matomoTrackEvent } = useTracking();
 
   const onClick = () => {
