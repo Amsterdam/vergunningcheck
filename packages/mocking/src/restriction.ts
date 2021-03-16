871e3605a96db4e3764fd0eff00f2b35a5c6969d
Sven Jens <sven@sven-jens.nl>
2021-03-16 07:26:18 +0100

    exteded list of zipcodes

--- packages/mocking/src/restriction.ts
+++ packages/mocking/src/restriction.ts
@@ -3,15 +3,19 @@ import { ZipCode, HouseNumberFull } from "./generic";
 export const NO_MONUMENT = "NO_MONUMENT";
 export const NATIONAL_MONUMENT = "NATIONAL_MONUMENT";
 export const MUNICIPAL_MONUMENT = "MUNICIPAL_MONUMENT";
+export const MUNICIPAL_MONUMENT_PROCEDURE = "MUNICIPAL_MONUMENT_PROCEDURE";
 export const NO_CITY_SCAPE = "NO_CITY_SCAPE";
 export const NATIONAL_CITY_SCAPE = "NATIONAL_CITY_SCAPE";
 export const MUNICIPAL_CITY_SCAPE = "MUNICIPAL_CITY_SCAPE";
+export const METRO_STATION = "METRO_STATION"; // Metro station
 
 export type RestrictionProperty =
   | "NO_MONUMENT"
   | "NATIONAL_MONUMENT"
   | "MUNICIPAL_MONUMENT"
+  | "MUNICIPAL_MONUMENT_PROCEDURE"
   | "NO_CITY_SCAPE"
+  | "METRO_STATION"
   | "NATIONAL_CITY_SCAPE"
   | "MUNICIPAL_CITY_SCAPE";
 
@@ -63,6 +67,12 @@ export const fixtures = [
   ["1012TK", "1 A", [NATIONAL_MONUMENT, NATIONAL_CITY_SCAPE]],
   ["1017GW", "279 H", [NATIONAL_MONUMENT, NATIONAL_CITY_SCAPE]],
   ["1073JP", 1, [NATIONAL_MONUMENT, NATIONAL_CITY_SCAPE]],
+
+  // Address of metro station
+  ["1018XA", 1, [METRO_STATION, NATIONAL_CITY_SCAPE]],
+
+  // In procedure munical monument:
+  ["1091XN", "17 2", [MUNICIPAL_MONUMENT_PROCEDURE]],
 ] as RestrictionFixture[];
 
 // Get unique array from fixtures based on restrictions
