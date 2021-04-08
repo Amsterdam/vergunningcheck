af977646aa8b31b3919412fededba3ab4a7caa42
André Lambert <afj.lambert@gmail.com>
2021-04-08 11:01:48 +0200

    Commit WIP

--- pocs/form-flow/test-api.js
+++ pocs/form-flow/test-api.js
@@ -3,12 +3,14 @@ const args = process.argv;
 
 const options = {
   method: args[3].toUpperCase(),
-  body: `activiteitId=${args[4]}&id=${args[4]}`,
+  body: `id=${args[4]}`,
   headers: {
     "x-api-key": process.env.STTR_BUILDER_API_KEY,
     "content-type": "application/x-www-form-urlencoded",
   },
 };
+
+console.log(options.method, args[2], options.body);
 
 fetch(args[2], options)
   .then((res) => res.json())
