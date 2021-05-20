a181afff698cd9ae8f88979290a3d612a661db9b
Sven Jens <sven@sven-jens.nl>
2021-05-20 14:01:06 +0200

    selenium updates

--- packages/e2e/nightwatch/tests/imtrFlow.js
+++ packages/e2e/nightwatch/tests/imtrFlow.js
@@ -1,21 +1,22 @@
-const { NightwatchBrowser } = require("nightwatch");
 const { address, domain, selectors } = require("../config");
 
 const {
+  outcomeContact,
+  outcomeNeedPermitButton,
+  introBullets,
   locationHouseNumberFull,
   locationFound,
   locationPostalCode,
   locationRestrictionMonument,
   main,
   navButtonNext,
-  pq1a1,
-  pq1a2,
   q1a1,
   q1a2,
+  q2a1,
   questionAlert,
+  questionEditButton,
   questionForm,
 } = selectors;
-
 
 module.exports = {
   [__filename]: async (b) => {
@@ -23,16 +24,17 @@ module.exports = {
     b.url(`${domain}/dakkapel-plaatsen`);
 
     // Intro page has loaded
+    b.waitForElementVisible(introBullets);
     assert.containsText(
       main,
-      "Met de vergunningcheck kunt u zien of u een omgevingsvergunning nodig hebt."
+      "Met de vergunningcheck kunt u zien wanneer u een omgevingsvergunning nodig hebt."
     );
     b.click(navButtonNext);
 
     // Location Input has loaded
     assert.containsText(
       main,
-      "Voer het adres in waar u de dakkapel wilt gaan plaatsen."
+      "Voer het adres in waar u de dakkapel wilt gaan plaatsen"
     );
     assert.visible(locationPostalCode);
     b.setValue(locationPostalCode, address.zipCode);
@@ -47,36 +49,63 @@ module.exports = {
 
     // Question 1 has loaded
     b.waitForElementVisible(questionForm);
-    b.waitForElementVisible(pq1a1);
-    b.waitForElementVisible(pq1a2);
-    b.click(pq1a1);
-    b.click(navButtonNext);
     b.waitForElementVisible(q1a1);
     b.waitForElementVisible(q1a2);
-    // assert.containsText(main, "Het gebouw is een gemeentelijk monument.");
-    // assert.containsText(main, "Gaat u een dakkapel plaatsen of vernieuwen?");
-    // assert.containsText(main, "Volgende vraag");
-    // b.click(q1a1);
-    // b.waitForElementVisible(questionAlert);
-    // b.click(navButtonNext);
+    assert.containsText(main, "Het gebouw is een gemeentelijk monument.");
+    assert.containsText(
+      main,
+      "Staat het gebouw waarop u de dakkapel gaat plaatsen in een gebied"
+    );
+    assert.containsText(main, "Ja");
+    assert.containsText(main, "Nee");
+    b.click(q1a2);
+    b.waitForElementVisible(questionAlert);
+    assert.containsText(
+      main,
+      "Door dit antwoord kunnen we niet vaststellen of u een vergunning nodig hebt."
+    );
+    assert.containsText(main, "Naar de uitkomst");
+    b.click(navButtonNext);
 
-    // // Permit outcome has loaded
-    // b.waitForElementVisible(outcomeNeedPermitButton);
-    // assert.containsText(main, "U hebt een omgevingsvergunning nodig.");
-    // assert.containsText(main, "Zo werkt aanvragen");
-    // assert.containsText(main, "Uitkomst opslaan");
-    // assert.containsText(main, "Nog een vergunningcheck doen");
-    // b.click(outcomeNeedPermitButton);
-    //
-    // // The page `HOW_TO_APPLY_FOR_A_PERMIT` has loaded
-    // const result = await b.windowHandles();
-    // @ts-ignore
-    // b.switchWindow(result.value[1]);
-    // // TODO: make this URL available in packages/mocking
-    // assert.urlEquals(
-    //   "https://www.amsterdam.nl/veelgevraagd/?productid=%7B215DE049-EFA3-492D-A4B1-EDFF40E0BC51%7D#case_%7BBB9A7369-D795-42F8-8505-DC83EE4CC0C0%7D"
-    // );
-    //
+    // Contact outcome has loaded
+    b.waitForElementVisible(outcomeContact);
+    assert.containsText(main, "Neem contact op met de gemeente");
+    assert.containsText(main, "Nog een vergunningcheck doen");
+    b.click(questionEditButton);
+
+    // Question q1 has loaded (again)
+    b.waitForElementVisible(questionForm);
+    assert.containsText(main, "Naar de uitkomst");
+    b.click(q1a1);
+    assert.containsText(main, "Volgende vraag");
+    b.click(navButtonNext);
+
+    // Question 2 has loaded
+    b.waitForElementVisible(questionForm);
+    assert.containsText(main, "Gaat u een dakkapel plaatsen of vernieuwen?");
+    assert.containsText(main, "Volgende vraag");
+    b.click(q2a1);
+    b.waitForElementVisible(questionAlert);
+    assert.containsText(main, "Door dit antwoord hebt u een vergunning nodig.");
+    assert.containsText(main, "Naar de uitkomst");
+    b.click(navButtonNext);
+
+    // Permit outcome has loaded
+    b.waitForElementVisible(outcomeNeedPermitButton);
+    assert.containsText(main, "U hebt een omgevingsvergunning nodig.");
+    assert.containsText(main, "Zo werkt aanvragen");
+    assert.containsText(main, "Uitkomst opslaan");
+    assert.containsText(main, "Nog een vergunningcheck doen");
+    b.click(outcomeNeedPermitButton);
+
+    // The page `HOW_TO_APPLY_FOR_A_PERMIT` has loaded
+    const result = await b.windowHandles();
+    b.switchWindow(result.value[1]);
+    // TODO: make this URL available in packages/mocking
+    assert.urlEquals(
+      "https://www.amsterdam.nl/veelgevraagd/?productid=%7B215DE049-EFA3-492D-A4B1-EDFF40E0BC51%7D#case_%7BBB9A7369-D795-42F8-8505-DC83EE4CC0C0%7D"
+    );
+
     /**
      *
      * We can extend this test with:
